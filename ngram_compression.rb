f0caf52dae569c5978be37d069009cdf66fbf12e
TakedaKosuke <june.bure@gmail.com>
2017-01-31 00:08:48 +0900

    import w2

--- ngram_compression.rb
+++ ngram_compression.rb
@@ -1,3 +1,4 @@
+
 class NgramCompression
 end
 
@@ -9,3 +10,14 @@ open('cantrbry/alice29.txt') do |io|
 end
 words = str.split(/[\s,.;:`()]/) - [""]
 
+
+require 'csv'
+#辞書作成　
+#符号化 [word1,word2] > rank
+#復号 [word1,rank] > word2
+dic = {}
+CSV.foreach('w2-s.tsv', :col_sep => "\t") do |row|
+  dic[[row[0],row[1]]] = row[2].to_i
+end
+
+p dic

f1edf6e8ed242cecf6c02ee042efca6a6ac1e6cf
TakedaKosuke <june.bure@gmail.com>
2017-05-31 19:04:15 +0900

    word2

--- dictionary.rb
+++ dictionary.rb
@@ -1,8 +1,8 @@
 input = ARGV[0]
 output = ARGV[1]
 
-$stdin = open(input, "r")
-$stdout = open(output, "w") if ARGV[1]
+$stdin = open(input, "rb")
+$stdout = open(output, "wb") if ARGV[1]
 
 def web1gm
   freq_threadshold = 10000
@@ -24,11 +24,14 @@ def web2gm
   while input = gets do
     str,freqstr = input.split("\t")
     freq = freqstr.to_i
-    words = str.split
-    dic[words] = freq if freq > freq_threadshold
+    word1,word2 = str.split
+    dic[word1] = {} if dic[word1] == nil
+    dic[word1][word2] = freq
   end
-  dic = dic.sort{ |(k1,v1),(k2,v2)| v2 <=> v1}
-  dic.each_with_index { |(k, v),i| puts "#{k.join("\t")}\t#{i+1}" }
+  dic.each do |word1,word2_dic|
+    ranks = word2_dic.sort{ |(k1,v1),(k2,v2)| v2 <=> v1}
+    ranks.each_with_index { |(k, v),i| puts "#{word1}\t#{k}\t#{i+1}" }
+  end
 end
 
 web2gm

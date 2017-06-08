f9158d21c50c40d575988efcdd11d7ef5e2202eb
TakedaKosuke <june.bure@gmail.com>
2017-06-08 13:28:54 +0900

    fix benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,12 @@
-!bin/sh
-ruby ngram_compression.rb cantrbry/alice29.txt >> result.log
-ruby ngram_compression.rb cantrbry/asyoulik.txt >> result.log
-ruby ngram_compression.rb cantrbry/cp.html >> result.log
-ruby ngram_compression.rb cantrbry/fields.c >> result.log
-ruby ngram_compression.rb cantrbry/grammar.lsp >> result.log
-ruby ngram_compression.rb cantrbry/lcet10.txt >> result.log
-ruby ngram_compression.rb cantrbry/plrabn12.txt >> result.log
+#!/bin/bash
+ruby ngram_compression.rb cantrbry/alice29.txt
+ruby ngram_compression.rb cantrbry/asyoulik.txt
+ruby ngram_compression.rb cantrbry/cp.html
+ruby ngram_compression.rb cantrbry/fields.c
+ruby ngram_compression.rb cantrbry/kennedy.xls
+ruby ngram_compression.rb cantrbry/grammar.lsp
+ruby ngram_compression.rb cantrbry/lcet10.txt
+ruby ngram_compression.rb cantrbry/plrabn12.txt
+ruby ngram_compression.rb cantrbry/ptt5
+ruby ngram_compression.rb cantrbry/sum
+ruby ngram_compression.rb cantrbry/xargs.1

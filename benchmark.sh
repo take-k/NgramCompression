2912b6921b4376455e36cfc78111304facbbcb12
TakedaKosuke <june.bure@gmail.com>
2017-06-08 12:31:19 +0900

    add benchmark

---
+++ benchmark.sh
@@ -0,0 +1,8 @@
+!bin/sh
+ruby ngram_compression.rb cantrbry/alice29.txt >> result.log
+ruby ngram_compression.rb cantrbry/asyoulik.txt >> result.log
+ruby ngram_compression.rb cantrbry/cp.html >> result.log
+ruby ngram_compression.rb cantrbry/fields.c >> result.log
+ruby ngram_compression.rb cantrbry/grammar.lsp >> result.log
+ruby ngram_compression.rb cantrbry/lcet10.txt >> result.log
+ruby ngram_compression.rb cantrbry/plrabn12.txt >> result.log

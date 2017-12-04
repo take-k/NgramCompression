630df6304b1b22d06cb5114848d9db04c097a911
TakedaKosuke <june.bure@gmail.com>
2017-12-04 14:01:20 +0900

    fix normalize

--- normalize.rb
+++ normalize.rb
@@ -4,21 +4,21 @@ def normalize
     open(file,'rb') do |f|
       sum = 0
       while (input = f.gets) do
-        row = input.split("\t")
+        row = input.chomp().split("\t")
         freq = row[-1].to_i
         sum += freq
       end
     end
 
-    shift = ARGV[1] || 1
+    shift = (ARGV[1].to_i) || 1
     return if shift == 0
 
     open("#{file}_output",'wb') do |output_f|
       open(file,'rb') do |input_f|
         while (input = input_f.gets) do
-          row = input.split("\t")
+          row = input.chomp().split("\t")
           words = row[0,row.size-1]
-          freq = row[-1].to_i <<= shift
+          freq = row[-1].to_i >> shift
           output_f.puts("#{words.join(" ")} #{freq}") if freq > 0
         end
       end
